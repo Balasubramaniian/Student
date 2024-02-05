3bfaa111da57068a66967b2983884e6aeb6fca87
bala15402 <balasubramaniyan.sel@gmail.com>
2024-02-05 19:19:49 +0530

    Create UserLogin.java

---
+++ Phase-3/Login/src/main/java/Login/UserLogin.java
@@ -0,0 +1,60 @@
+package Login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class UserLogin
+ */
+
+@WebServlet("/UserLogin")
+public class UserLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String name=request.getParameter("name");
+		int age=Integer.parseInt(request.getParameter("age"));
+		int id=Integer.parseInt(request.getParameter("id1"));
+		
+		Student s=new Student(id,name,age);
+		
+		DbConnection db=new DbConnection();
+		
+		try {
+			
+			db.prcreate(id, age, name);
+			response.sendRedirect("Login");
+		} 
+		catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+
+}
+}
